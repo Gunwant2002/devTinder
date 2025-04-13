47cd1814e61f2ac1dd7b9ffe1c0b065e222a7131
Gunwant2002 <gunvantbanait@gmail.com>
2025-04-13 15:13:43 +0530

    wildcatd-error-handling

--- src/app.js
+++ src/app.js
@@ -1,28 +1,31 @@
 const express = require("express");
 
 const app = express();
-const {adminAuth, userAuth} =require("./middlewares/auth");
 
 
-app.use("/admin",adminAuth);
-// app.use("/user",userAuth);
-app.get("/admin/getAllData", (req,res)=>{
-    //logic to get all data
-    res.send("Get all User data");
+app.use("/",(err,req,res,next)=>{
+    res.status(500).send("Something went wrong");
 })
 
-app.post("/user/login",(req,res)=>{
-    res.send("user login Successfully");
+
+app.get("/getUserData",(req,res)=>{
+
+    // try{
+        throw new Error("adkjflah");
+        res.send("User Data Sent");
+    // }
+    // catch(err){
+    //     res.status(500).send("some error, contact support team");
+    // }
+   
 })
 
-app.get("/user/data",userAuth,(req,res)=>{
-    res.send("User Data Sent");
+app.use("/",(err,req,res,next)=>{
+    res.status(500).send("Something went wrong");
 })
 
-app.get("/admin/deleteUser", (req,res)=>{
-    //logic to delete user
-    res.send("Deleted User");
-})
+
+
 
 app.listen(7777, ()=>{
     console.log("Server is successfully listening on port 7777...");
