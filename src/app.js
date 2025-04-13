e0ab07880e44a62d075ff26795a7c6520b52e9ba
Gunwant2002 <gunvantbanait@gmail.com>
2025-04-13 10:53:15 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -3,10 +3,40 @@ const express = require("express");
 const app = express();
 
 
-app.get("/user/:userId/:name/:password",(req,res)=>{
-    console.log(req.params);
-    res.send({firstname:"Gunwant",lastName:"Banait"});
-})
+app.get(
+    "/user",
+
+   (req,res,next)=>{
+    console.log("Handling route handlers for user 1 ");
+    next();
+    // res.send("Response");
+},
+
+(req,res,next)=>{
+    console.log("handling the routes for user 2");
+    // res.send("2nd Response");
+    next();
+},
+   
+(req,res,next)=>{
+    console.log("handling the routes for user 3");
+    // res.send("3rd Response");
+    next();
+},
+
+(req,res,next)=>{
+    console.log("handling the routes for user 4");
+    // res.send("4th Response");
+    next();
+},
+   
+(req,res,next)=>{
+    console.log("handling the routes for user 5");
+    res.send("5th Response");
+    
+}
+
+);
 
 
 
